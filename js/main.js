ca6ba33f186c7af60d398f0c2e20902be3b0192d
IGOR <priz8090@gmail.com>
2026-07-23 05:45:13 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 const navbar = document.querySelector(".navbar");
 const logo = document.querySelector(".logo-svg use");
+const button = document.querySelector(".navbar-button");
 window.addEventListener("scroll", () => {
   if (this.scrollY > 1) {
     navbar.classList.add("navbar-light");
     logo.href.baseVal = "img/sprite.svg#logo"
+    navbar-button.classList.add('navbar-button-s');
+    navbar-button.classList.remove('navbar-button');
   } else {
     navbar.classList.remove("navbar-light");
     logo.href.baseVal = "img/sprite.svg#logo-light"
+    
+    navbar-button.classList.add('navbar-button');
+    navbar-button.classList.remove('navbar-button-s');
   }
 });

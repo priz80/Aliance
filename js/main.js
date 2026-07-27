f7798d3cb2b4499e047826e8e45c348656e86026
IGOR <priz8090@gmail.com>
2026-07-27 22:59:50 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,23 +3,43 @@ const logo = document.querySelector(".logo-svg use");
 const button = document.querySelector(".navbar-button");
 const mMenuToggle = document.querySelector(".mobile-menu-toggle");
 const menu = document.querySelector(".mobile-menu");
+
+const lightModeOn = (event) => {
+  navbar.classList.add("navbar-light");
+  logo.href.baseVal = "img/sprite.svg#logo";
+};
+const lightModeOff = (event) => {
+  navbar.classList.remove("navbar-light");
+  logo.href.baseVal = "img/sprite.svg#logo-light";
+};
+
+const openMenu = (event) => {
+  menu.classList.add("is-open");
+  mMenuToggle.classList.add('close-menu');
+  document.body.style.overflow = "hidden";
+  lightModeOn();
+};
+const closeMenu = (event) => {
+  menu.classList.remove("is-open");
+  mMenuToggle.classList.remove('close-menu');
+  document.body.style.overflow = "";
+  lightModeOff();
+};
+
 window.addEventListener("scroll", () => {
   if (this.scrollY > 1) {
-    navbar.classList.add("navbar-light");
-    logo.href.baseVal = "img/sprite.svg#logo"
-    navbar-button.classList.add('navbar-button-s');
-    navbar-button.classList.remove('navbar-button');
+    navbar - button.classList.add("navbar-button-s");
+    navbar - button.classList.remove("navbar-button");
   } else {
-    navbar.classList.remove("navbar-light");
-    logo.href.baseVal = "img/sprite.svg#logo-light"
-    
-    navbar-button.classList.add('navbar-button');
-    navbar-button.classList.remove('navbar-button-s');
+    navbar - button.classList.add("navbar-button");
+    navbar - button.classList.remove("navbar-button-s");
   }
 });
-
+window.addEventListener("scroll", () => {
+  this.scrollY > 1 ? lightModeOn() : lightModeOff();
+});
 
 mMenuToggle.addEventListener("click", (event) => {
   event.preventDefault();
-  menu.classList.toggle('is-open');
-})
+  menu.classList.contains("is-open") ? closeMenu() : openMenu();
+});

54414bfccc78e7429f4008370becdc8e24a1d517
IGOR <priz8090@gmail.com>
2026-07-28 02:25:53 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -49,7 +49,7 @@ mMenuToggle.addEventListener("click", (event) => {
 
 const swiper = new Swiper(".swiper", {
   speed: 400,
-  autoHeight: true,
+  /* autoHeight: true, */
   loop: true,
   slidesPerView: 5,
   navigation: {

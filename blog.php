b91dd7d38bbbecaa79625cad72980f109d5428ed
IGOR <priz8090@gmail.com>
2026-08-22 23:02:56 +0300

    Update blog.php

--- blog.php
+++ blog.php
@@ -1,7 +1,7 @@
     <?php
     $page_title = "Блог";
-    $header_style = "page-title-blog";
-    include_once('page-title.php') ?>
+    $header_style = "blog-title-image";
+    include_once('pagetitle-blog.php') ?>
     <?php include_once('modal.php') ?>
     <?php include_once('mobile_menu.php') ?>
     <?php include_once('navbar_light.php') ?>

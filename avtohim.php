3d547c3b8c209390574190da9cd1e87d4c93bc15
IGOR <priz8090@gmail.com>
2026-08-22 15:26:31 +0300

    Update avtohim.php

--- avtohim.php
+++ avtohim.php
@@ -1,7 +1,176 @@
     <?php
-    $page_title = "Автохимия";
+    $page_title = "Автомобильная химия";
     $header_style = "page-title-bg-gray";
     include_once('page-title.php') ?>
+    <section class="section founder">
+        <div class="container">
+            <div class="prodact-content-wrapper">
+                <img class="prodact-photo" src="./img/production.jpg" alt="prodaction">
+                <div class="prodact-content">
+                    <div class="seporator"></div>
+                    <h2 class="section-title">Контрактное производство автомобильной химии</h2>
+                    <img class="prodact-photo-mobile" src="./img/production.jpg" alt="prodaction">
+                    <div class="prodact-text">
+                        <p>
+                            Прежде всего, начало повседневной работы по формированию позиции однозначно фиксирует необходимость приоритизации разума над эмоциями. Кстати, представители современных социальных резервов неоднозначны и будут описаны максимально подробно.
+                            Но социально-экономическое развитие напрямую зависит от поставленных обществом задач! Безусловно, постоянный количественный рост и сфера нашей активности предоставляет широкие возможности для системы обучения кадров, соответствующей насущным потребностям. В целом, конечно, выбранный нами инновационный путь обеспечивает широкому кругу (специалистов) участие в формировании своевременного выполнения сверхзадачи. А ещё элементы политического процесса представлены в исключительно положительном свете. Высокий уровень вовлечения представителей целевой аудитории является четким доказательством простого факта: высокотехнологичная концепция общественного уклада создаёт предпосылки для глубокомысленных рассуждений. В своём стремлении повысить качество жизни, они забывают, что дальнейшее развитие различных форм деятельности, а также свежий взгляд на привычные вещи - безусловно открывает новые горизонты для поставленных обществом задач.
+                        </p>
+                        <ul class="prodact-list">
+
+                            <li class="prodact-list-item">
+                                <svg width="30" height="30" class="clients-list-icon">
+                                    <use xlink:href="img/sprite.svg#car"></use>
+                                </svg>
+                                Автомобильная химия
+                            </li>
+                            <li class="prodact-list-item">
+                                <svg width="30" height="30" class="clients-list-icon">
+                                    <use xlink:href="img/sprite.svg#eat"></use>
+                                </svg>
+                                Бытовая химия
+                            </li>
+                            <li class="prodact-list-item">
+                                <svg width="30" height="30" class="clients-list-icon">
+                                    <use xlink:href="img/sprite.svg#brush"></use>
+                                </svg>
+                                Дезинфицирующие средства
+                            </li>
+                            <li class="prodact-list-item">
+                                <svg width="30" height="30" class="clients-list-icon">
+                                    <use xlink:href="img/sprite.svg#cosmetic"></use>
+                                </svg>
+                                Пищевые аэрозоли
+                            </li>
+                            <li class="prodact-list-item">
+                                <svg width="30" height="30" class="clients-list-icon">
+                                    <use xlink:href="img/sprite.svg#car-cosm"></use>
+                                </svg>
+                                Косметическая продукция
+                            </li>
+                            <li class="prodact-list-item">
+                                <svg width="30" height="30" class="clients-list-icon">
+                                    <use xlink:href="img/sprite.svg#shirt"></use>
+                                </svg>
+                                Краски аэрозольные
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- <img class="prodact-photo" src="./img/med.jpg" alt="prodaction"> -->
+            </div>
+        </div>
+    </section>
+
+    <div class="container advant">
+      <div class="swiper advant-slider">
+        <ul class="swiper-wrapper header-features">
+          <li class="swiper-slide header-features-item">
+            <svg width="36" height="36">
+              <use xlink:href="img/sprite.svg#time"></use>
+            </svg>
+            <p class="advant-features-text">Непрерывная работа c 2017 года</p>
+          </li>
+          <li class="swiper-slide header-features-item">
+            <svg width="36" height="36">
+              <use xlink:href="img/sprite.svg#certificate"></use>
+            </svg>
+            <p class="advant-features-text">Вся продукция сертифицирована</p>
+          </li>
+          <li class="swiper-slide header-features-item">
+            <svg width="36" height="36">
+              <use xlink:href="img/sprite.svg#control"></use>
+            </svg>
+            <p class="advant-features-text">
+              Контроль качества на всех этапах
+            </p>
+          </li>
+          <li class="swiper-slide header-features-item">
+            <svg width="36" height="36">
+              <use xlink:href="img/sprite.svg#delivery"></use>
+            </svg>
+            <p class="advant-features-text">
+              Возможны поставки по всей России
+            </p>
+          </li>
+          <li class="swiper-slide header-features-item">
+            <svg width="36" height="36">
+              <use xlink:href="img/sprite.svg#speed"></use>
+            </svg>
+            <p class="advant-features-text">Оперативное производство</p>
+          </li>
+        </ul>
+
+        <div class="slider-buttons">
+          <div class="advant-button-prev">
+            <svg width="36" height="24">
+              <use xlink:href="img/sprite.svg#advant-arrow-prev"></use>
+            </svg>
+          </div>
+          <div class="advant-button-next">
+            <svg width="36" height="24">
+              <use xlink:href="img/sprite.svg#advant-arrow-next"></use>
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <section class="section founder">
+        <div class="container">
+            <div class="prodact-content-wrapper">
+                <div class="prodact-content">
+                    <div class="seporator"></div>
+                    <h2 class="section-title">Изготовление автохимии</h2>
+                    <img class="prodact-photo-mobile" src="./img/production.jpg" alt="prodaction">
+                    <div class="prodact-text">
+                        <p>
+                            Но современная методология разработки позволяет оценить значение прогресса профессионального сообщества. Есть над чем задуматься: тщательные исследования конкурентов набирают популярность среди определенных слоев населения, а значит, должны быть ассоциативно распределены по отраслям. В своём стремлении повысить качество жизни, они забывают, что дальнейшее развитие различных форм деятельности, а также свежий взгляд на привычные вещи - безусловно открывает новые горизонты для поставленных обществом задач.
+                        </p>
+                        <ul class="prodact-list">
+
+                            <li class="prodact-list-item">
+                                <svg width="30" height="30" class="clients-list-icon">
+                                    <use xlink:href="img/sprite.svg#car"></use>
+                                </svg>
+                                Автомобильная химия
+                            </li>
+                            <li class="prodact-list-item">
+                                <svg width="30" height="30" class="clients-list-icon">
+                                    <use xlink:href="img/sprite.svg#eat"></use>
+                                </svg>
+                                Бытовая химия
+                            </li>
+                            <li class="prodact-list-item">
+                                <svg width="30" height="30" class="clients-list-icon">
+                                    <use xlink:href="img/sprite.svg#brush"></use>
+                                </svg>
+                                Дезинфицирующие средства
+                            </li>
+                            <li class="prodact-list-item">
+                                <svg width="30" height="30" class="clients-list-icon">
+                                    <use xlink:href="img/sprite.svg#cosmetic"></use>
+                                </svg>
+                                Пищевые аэрозоли
+                            </li>
+                            <li class="prodact-list-item">
+                                <svg width="30" height="30" class="clients-list-icon">
+                                    <use xlink:href="img/sprite.svg#car-cosm"></use>
+                                </svg>
+                                Косметическая продукция
+                            </li>
+                            <li class="prodact-list-item">
+                                <svg width="30" height="30" class="clients-list-icon">
+                                    <use xlink:href="img/sprite.svg#shirt"></use>
+                                </svg>
+                                Краски аэрозольные
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <img class="prodact-photo" src="./img/avtohim.jpg" alt="prodaction">
+            </div>
+        </div>
+    </section>
     <?php include_once('modal.php') ?>
     <?php include_once('mobile_menu.php') ?>
     <?php include_once('navbar_light.php') ?>

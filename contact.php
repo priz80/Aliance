3d92ac4e1d7d17c6288e3afe9f86d6d3d4ac07a6
IGOR <priz8090@gmail.com>
2026-08-24 11:13:16 +0300

    Update contact.php

--- contact.php
+++ contact.php
@@ -14,7 +14,7 @@ include_once('page-title.php') ?>
 </a> -->
     <a href="tel:+74996861014" class="footer-phone">+7 (499) 686-10-14</a>
     <div class="footer-info">
-      <svg class="phone-svg" width="24" hight="24">
+      <svg class="phone-svg" width="24" height="24">
         <use xlink:href="img/sprite.svg#mark"></use>
       </svg>
       <address class="footer-info-address">
@@ -22,7 +22,7 @@ include_once('page-title.php') ?>
       </address>
     </div>
     <div class="footer-info">
-      <svg class="mail-svg" width="24" hight="24">
+      <svg class="mail-svg" width="24" height="24">
         <use xlink:href="img/sprite.svg#mail"></use>
       </svg>
       <a href="mailto:a.dragunov@tdaliance.ru" class="footer-info-email">a.dragunov@tdaliance.ru</a>

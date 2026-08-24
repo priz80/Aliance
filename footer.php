625f296acb65d7e2c591ea4e7584f8af088aa745
IGOR <priz8090@gmail.com>
2026-08-24 10:19:48 +0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -45,19 +45,19 @@
             <a href="avtohim.php" class="footer-menu-link">Автомобильная химия</a>
           </li>
           <li class="footer-menu-item">
-            <a href="/" class="footer-menu-link">Бытовая химия</a>
+            <a href="template.php" class="footer-menu-link">Бытовая химия</a>
           </li>
           <li class="footer-menu-item">
-            <a href="/" class="footer-menu-link">Дезинфицирующие средства</a>
+            <a href="template.php" class="footer-menu-link">Дезинфицирующие средства</a>
           </li>
           <li class="footer-menu-item">
-            <a href="/" class="footer-menu-link">Пищевые аэрозоли</a>
+            <a href="template.php" class="footer-menu-link">Пищевые аэрозоли</a>
           </li>
           <li class="footer-menu-item">
-            <a href="/" class="footer-menu-link">Косметическая продукция</a>
+            <a href="template.php" class="footer-menu-link">Косметическая продукция</a>
           </li>
           <li class="footer-menu-item">
-            <a href="/" class="footer-menu-link">Краски аэрозольные</a>
+            <a href="template.php" class="footer-menu-link">Краски аэрозольные</a>
           </li>
         </ul>
       </div>
@@ -65,10 +65,10 @@
         <h2 class="footer-menu-title">Собственные марки</h2>
         <ul class="footer-menu-list">
           <li class="footer-menu-item">
-            <a href="/" class="footer-menu-link">Автохимия AG-Tech</a>
+            <a href="template.php" class="footer-menu-link">Автохимия AG-Tech</a>
           </li>
           <li class="footer-menu-item">
-            <a href="/" class="footer-menu-link">Автохимия AP</a>
+            <a href="template.php" class="footer-menu-link">Автохимия AP</a>
           </li>
         </ul>
       </div>
@@ -78,7 +78,7 @@
             <a href="about.php" class="footer-menu-link footer-menu-link-bold">О компании</a>
           </li>
           <li class="footer-menu-item">
-            <a href="#" class="footer-menu-link footer-menu-link-bold">Новости</a>
+            <a href="news.php" class="footer-menu-link footer-menu-link-bold">Новости</a>
           </li>
           <li class="footer-menu-item">
             <a href="contact.php" class="footer-menu-link footer-menu-link-bold">Контакты</a>

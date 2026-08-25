d8360d257d0c7b08ab7237479d8da34915949601
IGOR <priz8090@gmail.com>
2026-08-25 23:04:49 +0300

    Create blog_more.php

---
+++ blog_more.php
@@ -0,0 +1,12 @@
+<?php
+/* $page_title = "Блог"; */
+$header_style = "blog-title-image";
+include_once('pagetitle-blog.php');
+
+
+include_once('modal.php');
+include_once('mobile_menu.php');
+include_once('navbar_light.php');
+include_once('cta.php');
+include_once('footer.php');
+?>

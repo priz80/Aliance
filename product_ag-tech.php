ac92ce64d29cfbeed4698a528dacf6c6da3f144f
IGOR <priz8090@gmail.com>
2026-08-27 09:38:18 +0300

    Create product_ag-tech.php

---
+++ product_ag-tech.php
@@ -0,0 +1,125 @@
+<?php
+$page_title = "Автохимия AG-Tech";
+$header_style = "page-title-bg-gray";
+include_once('page-title.php') ?>
+    
+<div class="container">
+      <section class="section founder">
+        <div class="container">
+            <div class="prodact-content-wrapper">
+                <img class="prodact-photo" src="./img/production.jpg" alt="prodaction">
+                <div class="prodact-content">
+                    <div class="seporator"></div>
+                    <h2 class="section-title">Контрактное производство автомобильной химии</h2>
+                    <img class="prodact-photo-mobile" src="./img/production.jpg" alt="prodaction">
+                    <div>
+                        <p>
+                            Прежде всего, начало повседневной работы по формированию позиции однозначно фиксирует необходимость приоритизации разума над эмоциями. Кстати, представители современных социальных резервов неоднозначны и будут описаны максимально подробно.
+                            Но социально-экономическое развитие напрямую зависит от поставленных обществом задач! Безусловно, постоянный количественный рост и сфера нашей активности предоставляет широкие возможности для системы обучения кадров, соответствующей насущным потребностям. В целом, конечно, выбранный нами инновационный путь обеспечивает широкому кругу (специалистов) участие в формировании своевременного выполнения сверхзадачи. А ещё элементы политического процесса представлены в исключительно положительном свете. Высокий уровень вовлечения представителей целевой аудитории является четким доказательством простого факта: высокотехнологичная концепция общественного уклада создаёт предпосылки для глубокомысленных рассуждений. В своём стремлении повысить качество жизни, они забывают, что дальнейшее развитие различных форм деятельности, а также свежий взгляд на привычные вещи - безусловно открывает новые горизонты для поставленных обществом задач.
+                        </p>
+                        <ul class="prodact-list">
+
+                            <li class="prodact-list-item">
+                                <svg width="30" height="30" class="clients-list-icon">
+                                    <use xlink:href="img/sprite.svg#car"></use>
+                                </svg>
+                                Автомобильная химия
+                            </li>
+                            <li class="prodact-list-item">
+                                <svg width="30" height="30" class="clients-list-icon">
+                                    <use xlink:href="img/sprite.svg#eat"></use>
+                                </svg>
+                                Бытовая химия
+                            </li>
+                            <li class="prodact-list-item">
+                                <svg width="30" height="30" class="clients-list-icon">
+                                    <use xlink:href="img/sprite.svg#brush"></use>
+                                </svg>
+                                Дезинфицирующие средства
+                            </li>
+                            <li class="prodact-list-item">
+                                <svg width="30" height="30" class="clients-list-icon">
+                                    <use xlink:href="img/sprite.svg#cosmetic"></use>
+                                </svg>
+                                Пищевые аэрозоли
+                            </li>
+                            <li class="prodact-list-item">
+                                <svg width="30" height="30" class="clients-list-icon">
+                                    <use xlink:href="img/sprite.svg#car-cosm"></use>
+                                </svg>
+                                Косметическая продукция
+                            </li>
+                            <li class="prodact-list-item">
+                                <svg width="30" height="30" class="clients-list-icon">
+                                    <use xlink:href="img/sprite.svg#shirt"></use>
+                                </svg>
+                                Краски аэрозольные
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- <img class="prodact-photo" src="./img/med.jpg" alt="prodaction"> -->
+            </div>
+        </div>
+    </section>
+
+      <div class="swiper swiper-slider">
+        <ul class="swiper-wrapper header-features">
+          <li class="swiper-slide header-features-item">
+            <svg width="36" height="36">
+              <use xlink:href="img/sprite.svg#time"></use>
+            </svg>
+            <p class="header-features-text">Непрерывная работа c 2017 года</p>
+          </li>
+          <li class="swiper-slide header-features-item">
+            <svg width="36" height="36">
+              <use xlink:href="img/sprite.svg#certificate"></use>
+            </svg>
+            <p class="header-features-text">Вся продукция сертифицирована</p>
+          </li>
+          <li class="swiper-slide header-features-item">
+            <svg width="36" height="36">
+              <use xlink:href="img/sprite.svg#control"></use>
+            </svg>
+            <p class="header-features-text">
+              Контроль качества на всех этапах
+            </p>
+          </li>
+          <li class="swiper-slide header-features-item">
+            <svg width="36" height="36">
+              <use xlink:href="img/sprite.svg#delivery"></use>
+            </svg>
+            <p class="header-features-text">
+              Возможны поставки по всей России
+            </p>
+          </li>
+          <li class="swiper-slide header-features-item">
+            <svg width="36" height="36">
+              <use xlink:href="img/sprite.svg#speed"></use>
+            </svg>
+            <p class="header-features-text">Оперативное производство</p>
+          </li>
+        </ul>
+
+        <div class="slider-buttons">
+          <div class="slider-button-prev">
+            <svg width="36" height="24">
+              <use xlink:href="img/sprite.svg#arrow-prev"></use>
+            </svg>
+          </div>
+          <div class="slider-button-next">
+            <svg width="36" height="24">
+              <use xlink:href="img/sprite.svg#arrow-next"></use>
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    
+    <?php include_once('modal.php') ?>
+    <?php include_once('mobile_menu.php') ?>
+    <?php include_once('navbar_light.php') ?>
+
+    <?php include_once('cta.php') ?>
+    <?php include_once('footer.php') ?>

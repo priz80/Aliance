40a988bec8b3bd411f45c93f42bc676eb107cab5
IGOR <priz8090@gmail.com>
2026-08-27 17:27:29 +0300

    Update own_product.php

--- own_product.php
+++ own_product.php
@@ -1,7 +1,7 @@
 <?php
 $page_title = "Собственные торговые марки";
-$header_style = "page-title-bg-gray";
-include_once('page-title.php') ?>
+$header_style = "page-header-bg-gray";
+include_once('page-header.php') ?>
     <?php include_once('./template/trademark-block.php') ?>
     <?php include_once('modal.php') ?>
     <?php include_once('mobile_menu.php') ?>

c803f5153060ff58a248fc8bb859ba1482b68b66
IGOR <priz8090@gmail.com>
2026-08-27 17:27:39 +0300

    Update policy.php

--- policy.php
+++ policy.php
@@ -1,7 +1,7 @@
 <?php
 $page_title = "Политика конфиденциальности";
-$header_style = "page-title-bg-gray";
-include_once('page-title.php') ?>
+$header_style = "page-header-bg-gray";
+include_once('page-header.php') ?>
 
 
 <div class="container policy">

746674bbf699d1c2c05e917b0b7df580e726a57e
IGOR <priz8090@gmail.com>
2026-08-27 22:17:24 +0300

    Update avtohim.php

--- avtohim.php
+++ avtohim.php
@@ -15,7 +15,7 @@
       // стили вывода изображения
       $image_visibility = " ";
       $image_mobile_visibility = "image-mobile_founder";
-    
+
 
       include_once('page-header.php');
       include_once('modal.php');
@@ -26,7 +26,58 @@
 
       <?php include_once('advant-slider-block.php') ?>
 
-      <?php include_once('right-image-block.php') ?>
+      <section class="section founder">
+
+        <div class="container">
+          <div class="avtohim-content-wrapper">
+            <div class="founder-content">
+              <div class="seporator"></div>
+              <h2 class="section-title"><?php echo $block_title_right ?></h2>
+              <p><?php echo $text_block_right ?></p>
+              <ul class="prodact-list">
+
+                <li class="prodact-list-item">
+                  <svg width="30" height="30" class="clients-list-icon">
+                    <use xlink:href="img/sprite.svg#car"></use>
+                  </svg>
+                  Автомобильная химия
+                </li>
+                <li class="prodact-list-item">
+                  <svg width="30" height="30" class="clients-list-icon">
+                    <use xlink:href="img/sprite.svg#eat"></use>
+                  </svg>
+                  Бытовая химия
+                </li>
+                <li class="prodact-list-item">
+                  <svg width="30" height="30" class="clients-list-icon">
+                    <use xlink:href="img/sprite.svg#brush"></use>
+                  </svg>
+                  Дезинфицирующие средства
+                </li>
+                <li class="prodact-list-item">
+                  <svg width="30" height="30" class="clients-list-icon">
+                    <use xlink:href="img/sprite.svg#cosmetic"></use>
+                  </svg>
+                  Пищевые аэрозоли
+                </li>
+                <li class="prodact-list-item">
+                  <svg width="30" height="30" class="clients-list-icon">
+                    <use xlink:href="img/sprite.svg#car-cosm"></use>
+                  </svg>
+                  Косметическая продукция
+                </li>
+                <li class="prodact-list-item">
+                  <svg width="30" height="30" class="clients-list-icon">
+                    <use xlink:href="img/sprite.svg#shirt"></use>
+                  </svg>
+                  Краски аэрозольные
+                </li>
+              </ul>
+            </div>
+            <img class="founder-photo-avtohim" src="<?php echo $image_block_right ?>" alt="founder">
+          </div>
+        </div>
+      </section>
 
       <?php include_once('./template/center.php') ?>
 

c88e4805cba510dc8f087193e54f8a13b222b785
IGOR <priz8090@gmail.com>
2026-08-27 17:27:49 +0300

    Create section-light.php

---
+++ section-light.php
@@ -0,0 +1,55 @@
+<section class="section section-light">
+    <div class="container">
+      <div class="seporator"></div>
+      <h2 class="section-title">схема работы</h2>
+
+      <div class="swiper steps-slider">
+        <ul class="swiper-wrapper steps">
+          <li class="swiper-slide steps-item">
+            <span class="steps-num">01</span>
+            <h3 class="steps-title">Знакомство</h3>
+            <p class="steps-text">
+              Безусловно, сплочённость команды профессионалов позволяет
+              оценить значение форм воздействия.
+            </p>
+            <a href="" class="button-link" data-toggle="modal" data-target="#feedback-modal"> Оставить заявку </a>
+          </li>
+          <li class="swiper-slide steps-item">
+            <span class="steps-num">02</span>
+            <h3 class="steps-title">Заключение договора</h3>
+            <p class="steps-text">
+              Лишь интерактивные прототипы призваны к ответу.
+            </p>
+          </li>
+          <li class="swiper-slide steps-item">
+            <span class="steps-num">03</span>
+            <h3 class="steps-title">Производство</h3>
+            <p class="steps-text">
+              А также стремящиеся вытеснить традиционное производство,
+              нанотехнологии функционально разнесены на независимые элементы.
+            </p>
+          </li>
+          <li class="swiper-slide steps-item">
+            <span class="steps-num">04</span>
+            <h3 class="steps-title">Доставка</h3>
+            <p class="steps-text">
+              В частности, экономическая повестка сегодняшнего дня говорит о
+              возможностях приоритизации разума над эмоциями.
+            </p>
+          </li>
+        </ul>
+        <div class="steps-buttons primary-buttons-wraper">
+          <div class="steps-button-prev primary-button-prev">
+            <svg width="36" height="24">
+              <use xlink:href="img/sprite.svg#arrow-prev"></use>
+            </svg>
+          </div>
+          <div class="steps-button-next primary-button-next">
+            <svg width="36" height="24">
+              <use xlink:href="img/sprite.svg#arrow-next"></use>
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>

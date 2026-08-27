fbce44673d88f7798d8bb2f211c7820d27bc1c34
IGOR <priz8090@gmail.com>
2026-08-27 17:27:27 +0300

    Update news.php

--- news.php
+++ news.php
@@ -1,7 +1,7 @@
 <?php
 $page_title = "Новости";
-$header_style = "page-title-bg-gray";
-include_once('page-title.php') ?>
+$header_style = "page-header-bg-gray";
+include_once('page-header.php') ?>
     
     <?php include_once('modal.php') ?>
     <?php include_once('mobile_menu.php') ?>

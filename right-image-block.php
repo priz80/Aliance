137e1fb17d27325cf01f1cc30e424ba962bfaef9
IGOR <priz8090@gmail.com>
2026-08-27 17:27:47 +0300

    Create right-image-block.php

---
+++ right-image-block.php
@@ -0,0 +1,54 @@
+    <section class="section founder">
+      <div class="container">
+        <div class="prodact-content-wrapper">
+          <div class="prodact-content">
+            <div class="seporator"></div>
+            <h2 class="section-title"><?php echo $block_title ?></h2>
+            <img class="prodact-photo-right <?php echo $image_mobile_visibility ?>" src="<?php echo $image_block_right ?>" alt="prodaction">
+            <div>
+              <p><?php echo $text_block ?></p>
+              <ul class="prodact-list">
+
+                <li class="prodact-list-item">
+                  <svg width="30" height="30" class="clients-list-icon">
+                    <use xlink:href="img/sprite.svg#car"></use>
+                  </svg>
+                  Автомобильная химия
+                </li>
+                <li class="prodact-list-item">
+                  <svg width="30" height="30" class="clients-list-icon">
+                    <use xlink:href="img/sprite.svg#eat"></use>
+                  </svg>
+                  Бытовая химия
+                </li>
+                <li class="prodact-list-item">
+                  <svg width="30" height="30" class="clients-list-icon">
+                    <use xlink:href="img/sprite.svg#brush"></use>
+                  </svg>
+                  Дезинфицирующие средства
+                </li>
+                <li class="prodact-list-item">
+                  <svg width="30" height="30" class="clients-list-icon">
+                    <use xlink:href="img/sprite.svg#cosmetic"></use>
+                  </svg>
+                  Пищевые аэрозоли
+                </li>
+                <li class="prodact-list-item">
+                  <svg width="30" height="30" class="clients-list-icon">
+                    <use xlink:href="img/sprite.svg#car-cosm"></use>
+                  </svg>
+                  Косметическая продукция
+                </li>
+                <li class="prodact-list-item">
+                  <svg width="30" height="30" class="clients-list-icon">
+                    <use xlink:href="img/sprite.svg#shirt"></use>
+                  </svg>
+                  Краски аэрозольные
+                </li>
+              </ul>
+            </div>
+          </div>
+          <img class="prodact-photo-right <?php echo $image_visibility ?>" src="<?php echo $image_block_right ?>" alt="prodaction">
+        </div>
+      </div>
+    </section>

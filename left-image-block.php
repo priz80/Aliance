62e192e4ed34531e16435a69b71152efe3c2e99e
IGOR <priz8090@gmail.com>
2026-08-27 17:27:23 +0300

    Create left-image-block.php

---
+++ left-image-block.php
@@ -0,0 +1,14 @@
+    <section class="section prodact">
+      <div class="container">
+        <div class="prodact-content-wrapper">
+          <img class="prodact-photo-left" src="<?php echo $image_block ?>" alt="prodaction">
+          <div class="prodact-content">
+            <div class="seporator"></div>
+            <h2 class="section-title"><?php echo $block_title ?></h2>
+            <div>
+              <p><?php echo $text_block ?></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>

9451752c9e9b5f9007a3dfd18c906851d8ff4fed
IGOR <priz8090@gmail.com>
2026-08-28 14:19:07 +0300

    Create clients.php

---
+++ template/clients.php
@@ -0,0 +1,139 @@
+<section class="section clients">
+  <div class="container">
+    <div class="seporator"></div>
+    <div class="clients-wrapper">
+      <div class="clients-content">
+        <h2 class="section-title">
+          Производим аэрозольную продукцию для разных сфер
+        </h2>
+        <ul class="clients-list">
+          <li class="clients-list-item">
+            <svg width="30" height="30" class="clients-list-icon">
+              <use xlink:href="img/sprite.svg#him"></use>
+            </svg>
+            Химические производства
+          </li>
+          <li class="clients-list-item">
+            <svg width="30" height="30" class="clients-list-icon">
+              <use xlink:href="img/sprite.svg#car"></use>
+            </svg>
+            Автомойки
+          </li>
+          <li class="clients-list-item">
+            <svg width="30" height="30" class="clients-list-icon">
+              <use xlink:href="img/sprite.svg#eat"></use>
+            </svg>
+            Пищевая продукция
+          </li>
+          <li class="clients-list-item">
+            <svg width="30" height="30" class="clients-list-icon">
+              <use xlink:href="img/sprite.svg#brush"></use>
+            </svg>
+            Лаки и краски
+          </li>
+          <li class="clients-list-item">
+            <svg width="30" height="30" class="clients-list-icon">
+              <use xlink:href="img/sprite.svg#cosmetic"></use>
+            </svg>
+            Косметические средства
+          </li>
+          <li class="clients-list-item">
+            <svg width="30" height="30" class="clients-list-icon">
+              <use xlink:href="img/sprite.svg#car-cosm"></use>
+            </svg>
+            Автомобильная косметика
+          </li>
+          <li class="clients-list-item">
+            <svg width="30" height="30" class="clients-list-icon">
+              <use xlink:href="img/sprite.svg#shirt"></use>
+            </svg>
+            Косметика по уходу за одеждой
+          </li>
+          <li class="clients-list-item">
+            <svg width="30" height="30" class="clients-list-icon">
+              <use xlink:href="img/sprite.svg#boots"></use>
+            </svg>
+            Косметика по уходу за обувью
+          </li>
+          <li class="clients-list-item">
+            <svg width="30" height="30" class="clients-list-icon">
+              <use xlink:href="img/sprite.svg#bricks"></use>
+            </svg>
+            Строительные материалы
+          </li>
+          <li class="clients-list-item">
+            <svg width="30" height="30" class="clients-list-icon">
+              <use xlink:href="img/sprite.svg#more"></use>
+            </svg>
+            И многих других
+          </li>
+        </ul>
+      </div>
+      <div class="clients-logo-list">
+        <a href="#" class="client-logo-item clients-logo" >
+          <picture>
+            <source type="image/webp" srcset="./img/client.webp">
+            <source type="image/jpeg" srcset="./img/client.jpg">
+            <img src="./img/client.jpg" alt="clients">
+          </picture>
+        </a>
+        <a href="#" class="client-logo-item clients-logo" >
+          <picture>
+            <source type="image/webp" srcset="./img/client.webp">
+            <source type="image/jpeg" srcset="./img/client.jpg">
+            <img src="./img/client.jpg" alt="clients">
+          </picture>
+        </a>
+        <a href="#" class="client-logo-item clients-logo" >
+          <picture>
+            <source type="image/webp" srcset="./img/client.webp">
+            <source type="image/jpeg" srcset="./img/client.jpg">
+            <img src="./img/client.jpg" alt="clients">
+          </picture>
+        </a>
+        <a href="#" class="client-logo-item clients-logo" >
+          <picture>
+            <source type="image/webp" srcset="./img/client.webp">
+            <source type="image/jpeg" srcset="./img/client.jpg">
+            <img src="./img/client.jpg" alt="clients">
+          </picture>
+        </a>
+        <a href="#" class="client-logo-item clients-logo" >
+          <picture>
+            <source type="image/webp" srcset="./img/client.webp">
+            <source type="image/jpeg" srcset="./img/client.jpg">
+            <img src="./img/client.jpg" alt="clients">
+          </picture>
+        </a>
+        <a href="#" class="client-logo-item clients-logo" >
+          <picture>
+            <source type="image/webp" srcset="./img/client.webp">
+            <source type="image/jpeg" srcset="./img/client.jpg">
+            <img src="./img/client.jpg" alt="clients">
+          </picture>
+        </a>
+        <a href="#" class="client-logo-item clients-logo" >
+          <picture>
+            <source type="image/webp" srcset="./img/client.webp">
+            <source type="image/jpeg" srcset="./img/client.jpg">
+            <img src="./img/client.jpg" alt="clients">
+          </picture>
+        </a>
+        <a href="#" class="client-logo-item clients-logo" >
+          <picture>
+            <source type="image/webp" srcset="./img/client.webp">
+            <source type="image/jpeg" srcset="./img/client.jpg">
+            <img src="./img/client.jpg" alt="clients">
+          </picture>
+        </a>
+        <a href="#" class="client-logo-item clients-logo" >
+          <picture>
+            <source type="image/webp" srcset="./img/client.webp">
+            <source type="image/jpeg" srcset="./img/client.jpg">
+            <img src="./img/client.jpg" alt="clients">
+          </picture>
+        </a>
+      </div>
+    </div>
+  </div>
+</section>

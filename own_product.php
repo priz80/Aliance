7c2acf19df600252f732ecec5a14e57a0dc0017a
IGOR <priz8090@gmail.com>
2026-08-28 17:34:18 +0300

    Update own_product.php

--- own_product.php
+++ own_product.php
@@ -2,10 +2,12 @@
 $page_title = "Собственные торговые марки";
 $header_style = "page-header-bg-gray";
 include_once('./template/page-header.php') ?>
-    <?php include_once('./template/trademark-block.php') ?>
-    <?php include_once('modal.php') ?>
-    <?php include_once('./template/mobile_menu.php') ?>
-    <?php include_once('./template/navbar_light.php') ?>
 
-    <?php include_once('./template/cta.php') ?>
-    <?php include_once('./template/footer.php') ?>
+<h1 class="page-title-chapter">Собственные торговые марки</h1>
+<?php include_once('./template/trademark-block.php') ?>
+<?php include_once('./template/modal.php') ?>
+<?php include_once('./template/mobile_menu.php') ?>
+<?php include_once('./template/navbar_light.php') ?>
+
+<?php include_once('./template/cta.php') ?>
+<?php include_once('./template/footer.php') ?>

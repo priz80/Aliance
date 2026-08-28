7501cc16628cd85a8519cc3ddbd0b86d492da4d3
IGOR <priz8090@gmail.com>
2026-08-28 14:41:32 +0300

    Update 404.php

--- 404.php
+++ 404.php
@@ -42,8 +42,8 @@ http_response_code(404);
   </div>
 
 
-  <?php include_once('modal.php') ?>
-  <?php include_once('mobile_menu.php') ?>
-  <?php include_once('navbar_light.php') ?>
+  <?php include_once('./template/modal.php') ?>
+  <?php include_once('./template/mobile_menu.php') ?>
+  <?php include_once('./template/navbar_light.php') ?>
 
-  <?php include_once('footer.php') ?>
+  <?php include_once('./template/footer.php') ?>

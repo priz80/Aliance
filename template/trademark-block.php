9098b92b8e84227baa9709ce33f664c810f940b7
IGOR <priz8090@gmail.com>
2026-08-28 23:14:22 +0300

    Update trademark-block.php

--- template/trademark-block.php
+++ template/trademark-block.php
@@ -8,28 +8,32 @@
     ?>
 
     <div class="trademark-cards">
-      <div class="trademark-card">
-        <svg class="trademark-card-logo">
-          <use xlink:href="img/sprite.svg#AGTECH"></use>
-        </svg>
-        <h2 class="trademark-card-title">Автохимия AG-Tech</h2>
-        <p class="trademark-card-text">
-          Для современного мира разбавленное изрядной долей эмпатии,
-          рациональное мышление создаёт предпосылки для поставленных
-          обществом задач.
-        </p>
-      </div>
-      <div class="trademark-card">
-        <svg class="trademark-card-logo">
-          <use xlink:href="img/sprite.svg#AP"></use>
-        </svg>
-        <h2 class="trademark-card-title">Автохимия AP</h2>
-        <p class="trademark-card-text">
-          Для современного мира разбавленное изрядной долей эмпатии,
-          рациональное мышление создаёт предпосылки для поставленных
-          обществом задач.
-        </p>
-      </div>
+      <a href="product_ag-tech.php">
+        <div class="trademark-card">
+          <svg class="trademark-card-logo">
+            <use xlink:href="img/sprite.svg#AGTECH"></use>
+          </svg>
+          <h2 class="trademark-card-title">Автохимия AG-Tech</h2>
+          <p class="trademark-card-text">
+            Для современного мира разбавленное изрядной долей эмпатии,
+            рациональное мышление создаёт предпосылки для поставленных
+            обществом задач.
+          </p>
+        </div>
+      </a>
+      <a href="template.php">
+        <div class="trademark-card">
+          <svg class="trademark-card-logo">
+            <use xlink:href="img/sprite.svg#AP"></use>
+          </svg>
+          <h2 class="trademark-card-title">Автохимия AP</h2>
+          <p class="trademark-card-text">
+            Для современного мира разбавленное изрядной долей эмпатии,
+            рациональное мышление создаёт предпосылки для поставленных
+            обществом задач.
+          </p>
+        </div>
+      </a>
     </div>
   </div>
 </section>

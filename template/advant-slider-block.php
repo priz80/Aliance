65d77fa4cc0df503af6cf3b80988a42a33443c27
IGOR <priz8090@gmail.com>
2026-08-28 14:13:35 +0300

    Create advant-slider-block.php

---
+++ template/advant-slider-block.php
@@ -0,0 +1,53 @@
+<div class="container-sw">
+      <div class="swiper swiper-slider">
+          <ul class="swiper-wrapper header-features">
+            <li class="swiper-slide header-features-item">
+              <svg width="36" height="36">
+                <use xlink:href="img/sprite.svg#time"></use>
+              </svg>
+              <p class="header-features-text">Непрерывная работа c 2017 года</p>
+            </li>
+            <li class="swiper-slide header-features-item">
+              <svg width="36" height="36">
+                <use xlink:href="img/sprite.svg#certificate"></use>
+              </svg>
+              <p class="header-features-text">Вся продукция сертифицирована</p>
+            </li>
+            <li class="swiper-slide header-features-item">
+              <svg width="36" height="36">
+                <use xlink:href="img/sprite.svg#control"></use>
+              </svg>
+              <p class="header-features-text">
+                Контроль качества на всех этапах
+              </p>
+            </li>
+            <li class="swiper-slide header-features-item">
+              <svg width="36" height="36">
+                <use xlink:href="img/sprite.svg#delivery"></use>
+              </svg>
+              <p class="header-features-text">
+                Возможны поставки по всей России
+              </p>
+            </li>
+            <li class="swiper-slide header-features-item">
+              <svg width="36" height="36">
+                <use xlink:href="img/sprite.svg#speed"></use>
+              </svg>
+              <p class="header-features-text">Оперативное производство</p>
+            </li>
+          </ul>
+  
+          <div class="slider-buttons">
+            <div class="slider-button-prev">
+              <svg width="36" height="24" fill='<?php echo $color_arrow ?>'>
+                <use xlink:href="img/sprite.svg#arrow-prev"></use>
+              </svg>
+            </div>
+            <div class="slider-button-next">
+              <svg width="36" height="24" fill='<?php echo $color_arrow ?>'>
+                <use xlink:href="img/sprite.svg#arrow-next"></use>
+              </svg>
+            </div>
+          </div>
+      </div>
+    </div>
